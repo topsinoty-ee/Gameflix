c0344f0979e29163852e924ce3618720b268c7d9
Promise Temitope <oluwatobilobatemi05@gmail.com>
2023-01-13 00:24:51 +0200

    Added css

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,33 +1,36 @@
-import React from 'react';
+import React from 'react'
 
 //Make a function MovieList() that maps through the movie database array from @MJOMA09 and makes a card that is rendered into the DOM
 
 const Movielist = (props) => {
-    let array = props.array;
-  
-    return array.map((m) => {
-      //Card
-      return (
-        <div>
-          <div>
-            <img src={m.src} alt={m.title} />
-          </div>
-          <div>
-            <h3>
-              <span>{m.title}</span>
-            </h3>
-            {/* tags */}
-            <ul>
-              {m.tags.map((e) =>{
-                return(
-                <li>{e.tag}</li>
-                  )
-              })}
-            </ul>
-          </div>
-        </div>
-      );
-    });
-  };
+  let array = props.array
 
-  export default Movielist;
+  return array.map((card) => {
+    //Card
+    return (
+      <div className="grid grid-rows-[2fr,1fr] border-2 border-black rounded-lg bg-white">
+        <div className="flex place-content-center items-center content-center">
+          <img src={card.src} alt={card.title} className="object-cover" />
+        </div>
+
+        <div className="mx-auto">
+          <h3 className="text-xl">{card.title}</h3>
+          {/* taglist */}
+          <ul className="flex flex-row flex-nowrap first:after:content-['&bull;'] last:before:content-['&bull;']">
+            {card.tags.map((selected) => {
+              const keyVal = `${card.title}-tag${selected.id}`
+
+              return (
+                <li key={keyVal} className="px-1 hover:underline hover:text-blue-800 transition-all duration-300">
+                  {selected.tag}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  })
+}
+
+export default Movielist
