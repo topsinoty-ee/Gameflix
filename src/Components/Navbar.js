7134415eb13181aec82b12829e4a3ab0cd540a37
Promise Temitope <oluwatobilobatemi05@gmail.com>
2023-02-17 19:45:38 +0200

    made class component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,39 +1,46 @@
 import React from 'react'
-import array from './Data_Array'
-import {ReactComponent as Logo} from '../Assets/Logo-text.svg'
+import Logo from '../Assets/Logo-text.svg'
 
-const Navbar = () => {
-  function Search() {
-    var input, filter, list, card, title, i, txtValue
-    input = document.getElementById('input')
-    filter = input.value.toUpperCase()
-    list = document.getElementById('list')
-    card = list.getElementsByClassName('card')
+class Navbar extends React.Component() {
+  constructor(props){
+    super(props)
+  }
+  Search(x) {
+    let input = x.value
+    let filter = input.toUpperCase()
+    let card = document.querySelectorAll('.card')
+    let title
 
-    for (i = 0; i < card.length; i++) {
-      title = card[i].getElementsByTagName('h3')[0]
-      txtValue = title.textContent || title.innerText
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        card[i].style.display = ''
+    for (let i = 0; i < card.length; i++) {
+      let selected = i
+      title = card[selected].getElementsByTagName('h3')
+      if (
+        title.innerText.toUpperCase().indexOf(filter) > -1 ||
+        title.textContent.toUpperCase().indexOf(filter) > -1
+      ) {
+        card[selected].style.display = ''
       } else {
-        card[i].style.display = 'none'
+        card[selected].style.display = 'none'
       }
     }
   }
 
-  return (
-    <header className='h-[10vh] bg-slate-700 flex align-center justify-between items-center px-5 py-2'>
-    <Logo/>
-      <input
-        placeholder="Search for games..."
-        type="text"
-        autoComplete="on"
-        list="Gamelist"
-        id="input"
-        onKeyDown={Search}
-        className="p-2 rounded-md"
-      />
-    </header>
-  )
+  render() {
+    return (
+      <header className="h-[10vh] bg-slate-700 flex align-center justify-between items-center px-5 py-2">
+        <img src={Logo} alt="logo"/>
+        <input
+          placeholder="Search for games..."
+          type="text"
+          autoComplete="on"
+          list="Gamelist"
+          id="input"
+          onKeyDown={this.Search(this)}
+          className="p-2 rounded-md"
+        />
+      </header>
+    )
+  }
 }
+
 export default Navbar
