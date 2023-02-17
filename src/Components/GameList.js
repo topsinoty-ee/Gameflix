d7fc7a4023e1d9d4c976d6bf6c5b9f11911f0e81
Promise Temitope <oluwatobilobatemi05@gmail.com>
2023-02-17 20:31:37 +0200

    made search bar component

--- src/Components/GameList.js
+++ src/Components/GameList.js
@@ -3,8 +3,8 @@ import Card from './Card'
 
 //Make a function GameList() that maps through the game database array from @MJOMA09 and makes a card that is rendered into the DOM
 
-const GameList = (props) => {
-  let array = props.data
+const GameList = ({data}) => {
+  let array = data
 
   return (
     <div className="grid grid-cols-4 p-px gap-3  overflow-scroll" id="list">
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const SearchBar = ({ value, onChange }) => {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search for games..."
+          value={value}
+          onChange={onChange}
+          className="p-2 rounded-md"
+        />
+      </div>
+    )
+  }
+  
